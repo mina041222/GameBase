b6a09535841ef66e96656b76becb60d2d5b936ed
junhee4613 <junhee000123@naver.com>
2023-08-16 15:57:44 +0900

    뭔가 시스템에 관한 것들!!

---
+++ GameBase-main/GameBase/Assets/Scripts/Core/Managers.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;
+    static Managers Instance { get { Init(); return s_instance; } }
+    public static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);  //Scene 이 종료되도 파괴 되지 않게 
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+
+    PoolManager _pool = new PoolManager();
+    ResourceManager _resource = new ResourceManager();
+    ObjectManager _object = new ObjectManager();
+    UIManager _ui = new UIManager();
+    GameManager _game = new GameManager();
+
+    public static UIManager UI { get { return Instance?._ui; } }
+    public static PoolManager Pool { get { return Instance?._pool; } }
+    public static ResourceManager Resource { get { return Instance?._resource; } }
+    public static ObjectManager Object { get { return Instance?._object; } }
+    public static GameManager Game { get { return Instance?._game; } }
+
+}
---
+++ GameBase-main/GameBase/Assets/Scripts/TestCode/TestScene.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class TestScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int Count = 0;
+    void Start()
+    {
+       Managers.Resource.LoadAllAsync<Object>("PreLoad", (key, count, totalCount) =>
+       {
+           Debug.Log("key : " + key + " Count : " + count + " totalCount : " + totalCount);
+           if(count == totalCount)
+           {
+               Managers.Game.Init();
+           }
+          
+       });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F1))
+        {
+            Count++;
+            Managers.UI.ShowToast(Count.ToString());
+        }
+        if (Input.GetKeyUp(KeyCode.F3))
+        {
+            Managers.Object.Spawn<MonsterController>(new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10)));
+        }
+
+        if (Input.GetKeyUp(KeyCode.F5))
+        {
+            Managers.Game.Gold += 1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.F6))
+        {
+            Managers.Game.AddEquipment("1");
+        }
+
+        if (Input.GetKeyUp(KeyCode.F7))
+        {
+            Managers.Game.AddMaterialItem(ID_BRONZE_KEY, 1);
+            Managers.Game.AddMaterialItem(ID_SILVER_KEY, 1);
+            Managers.Game.AddMaterialItem(ID_GOLD_KEY, 1);
+        }
+    }
+}
